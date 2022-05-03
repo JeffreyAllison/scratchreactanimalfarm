81307d8033c99e0025491570591314f71d985aa7
JeffAllison <itsjallison@gmail.com>
2022-05-02 17:27:08 -0700

    refactored to move data imports to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
+import './Main/Main.css';
 import Main from './Main/Main.js';
 import Header from './Header/Header.js';
 import Footer from './Footer/Footer.js';
+import React from 'react';
+import backgroundImg from './background.png';
+import { animals } from './data';
+import AnimalList from './AnimalList/AnimalList.js';
 
 function App() {
   return (
     <div className="App">
       <Header greeting="Welcome to the REACT Farm" />
 
-      <Main />
+      <Main backgroundImg={backgroundImg} />
+
+      <AnimalList animals={animals} />
 
       <Footer email="jeff@reactfarm.com" />
     </div>
--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,13 +1,3 @@
-import React from 'react';
-import backgroundImg from '../background.png';
-import { animals } from '../data';
-import './Main.css';
-import AnimalList from '../AnimalList/AnimalList.js';
-
-export default function Main() {
-  return (
-    <main style={{ backgroundImage: `url(${backgroundImg})` }}>
-      <AnimalList animals={animals} />
-    </main>
-  );
+export default function Main({ backgroundImg }) {
+  return <main style={{ backgroundImage: `url(${backgroundImg})` }}></main>;
 }
